5c82adbf9706c0c9d107b24f7ceece968849471d
sorarideblog <54093651+sorarideblog@users.noreply.github.com>
2020-09-11 08:43:05 +0900

    flutter [add] google auth

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/news_page.dart';
 import 'package:flutter_app/room_page.dart';
 import 'package:flutter_app/setting_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() {
   runApp(MyApp());
@@ -31,6 +34,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
+  bool _initialized = false;
+  bool _error = false;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   static List<Widget> _pageList = [
     RoomPage(),
@@ -38,14 +45,73 @@ class _MyHomePageState extends State<MyHomePage> {
     SettingPage()
   ];
 
+  void initializeFlutterFire() async {
+    try {
+      setState(() async {
+        await Firebase.initializeApp();
+
+        FirebaseAuth auth = FirebaseAuth.instance;
+        auth.authStateChanges().listen((User user) {
+          if (user == null) { // todo
+          } else {
+          }
+        });
+        signInWithGoogle().then((User user) => null);
+
+        _initialized = true;
+      });
+    } catch(e) {
+      setState(() {
+        _error = true;
+      });
+    }
+  }
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
+  Future<User> signInWithGoogle() async {
+    GoogleSignInAccount googleCurrentUser = _googleSignIn.currentUser;
+    try {
+      if (googleCurrentUser == null) googleCurrentUser = await _googleSignIn.signInSilently();
+      if (googleCurrentUser == null) googleCurrentUser = await _googleSignIn.signIn();
+      if (googleCurrentUser == null) return null;
+
+      GoogleSignInAuthentication googleAuth = await googleCurrentUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      final User user = (await _auth.signInWithCredential(credential)).user;
+      print("signed in " + user.displayName);
+
+      return user;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    if(_error) {
+      // return todo SomethingWentWrong();
+    }
+
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      // todo return Loading();
+    }
+
     return Scaffold(
       body: _pageList[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
