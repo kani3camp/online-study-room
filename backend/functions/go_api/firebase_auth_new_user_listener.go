6f23d54f3271c3b45aae18c1ff44a6a3aaa8db80
sorarideblog <sorarideblog@gmail.com>
2020-09-20 18:43:35 +0900

    api [fix] update FirebaseAuthNewUserListener

--- backend/functions/go_api/firebase_auth_new_user_listener.go
+++ backend/functions/go_api/firebase_auth_new_user_listener.go
@@ -1,6 +1,7 @@
 package go_api
 
 import (
+	"cloud.google.com/go/firestore"
 	"context"
 	"log"
 	"time"
@@ -13,12 +14,22 @@ type AuthEvent struct {
 	} `json:"metadata"`
 	UID string `json:"uid"`
 }
+
+func createNewUser(userId string, client *firestore.Client, ctx context.Context) (*firestore.WriteResult, error) {
+	return client.Collection(USERS).Doc(userId).Set(ctx, map[string]interface{}{
+		"registration-date": firestore.ServerTimestamp, // todo この書き方でいいんだっけ？
+		"last-access":       firestore.ServerTimestamp,
+		"online":            false,
+		"status":            "",
+	})
+}
+
 func FirebaseAuthNewUserListener(ctx context.Context, e AuthEvent)  error {
 	_, client := InitializeEventFunc()
 	defer client.Close()
 	
 	userId := e.UID
-	_, err := createNewUser(userId, "", client, ctx)
+	_, err := createNewUser(userId, client, ctx)
 	if err != nil {
 		log.Fatalln(err)
 	}
