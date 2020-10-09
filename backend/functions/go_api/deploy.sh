3a4ef452765ca46699c30fe11ef8158fbbd30b03
Sorarideblog <54093651+sorarideblog@users.noreply.github.com>
2020-10-10 02:08:41 +0900

    Update deploy.sh

--- backend/functions/go_api/deploy.sh
+++ backend/functions/go_api/deploy.sh
@@ -16,7 +16,7 @@ project_id="online-study-room-f1f30"
 #  --runtime go113 \
 #  --trigger-event providers/firebase.auth/eventTypes/user.create
 
-gcloud functions deploy UserStatus --trigger-http --runtime go113 --allow-unauthenticated
+gcloud functions deploy Rooms --trigger-http --runtime go113 --allow-unauthenticated
 gcloud functions deploy UpdateUserDoc --runtime go113 --trigger-event providers/cloud.firestore/eventTypes/document.update --trigger-resource "projects/online-study-room-f1f30/databases/(default)/documents/rooms/{roomId}"
 gcloud functions deploy FirebaseAuthNewUserListener --runtime go113 --trigger-event providers/firebase.auth/eventTypes/user.create
 
