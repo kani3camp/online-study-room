24f1bd5be0f3eb0a655390c4c25b7d6e54225ec9
sorarideblog <sorarideblog@gmail.com>
2020-11-06 17:59:34 +0900

    web [upgrade] sign in page

--- web_client/pages/sign_in.vue
+++ web_client/pages/sign_in.vue
@@ -11,6 +11,17 @@
         </v-flex>
       </v-container>
 
+      <v-container >
+        <p>　登録・サインインともに下のボタンから行えます。</p>
+        <v-row justify="center" class="google-sign-in-button">
+          <img v-bind:src="imageSource"
+               v-on:mouseover="changeImageToHovered"
+               v-on:mousedown="changeImageToPressed"
+               v-on:mouseout="changeImageToNormal"
+               alt="google sign in button">
+        </v-row>
+      </v-container>
+
     </v-main>
 
     <Footer></Footer>
@@ -18,11 +29,37 @@
 </template>
 
 <script>
+import normalImage from '~/assets/google_signin_buttons/web/2x/btn_google_signin_light_normal_web@2x.png'
+import hoveredImage from '~/assets/google_signin_buttons/web/2x/btn_google_signin_light_focus_web@2x.png'
+import pressedImage from '~/assets/google_signin_buttons/web/2x/btn_google_signin_light_pressed_web@2x.png'
+
 export default {
-name: "sign_in"
+  name: "sign_in",
+  data: () => ({
+    imageSource: '',
+  }),
+  created() {
+    this.imageSource = normalImage
+  },
+  methods: {
+    changeImageToHovered() {
+      console.log('fda')
+      this.imageSource = hoveredImage
+    },
+    changeImageToPressed() {
+      this.imageSource = pressedImage
+    },
+    changeImageToNormal() {
+      this.imageSource = normalImage
+    }
+  },
 }
 </script>
 
 <style scoped>
+
+.google-sign-in-button img,  :hover, :active {
+  max-height: 3rem;
+}
 
 </style>
