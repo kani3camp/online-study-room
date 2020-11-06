2615618926504b594d085e58b94c6e6c34c2acd3
Sorarideblog <54093651+sorarideblog@users.noreply.github.com>
2020-11-06 22:04:38 +0900

    web [upgrade] add link to items

--- web_client/components/Footer.vue
+++ web_client/components/Footer.vue
@@ -7,11 +7,11 @@
         <v-col cols="auto">
           <h4>SERVICE</h4>
           <ul>
-            <li>サイトトップ</li>
-            <li>このサイトについて</li>
-            <li>お知らせ</li>
-            <li>利用規約</li>
-            <li>プライバシーポリシー</li>
+            <li><nuxt-link to="/">サイトトップ</nuxt-link></li>
+            <li><nuxt-link to="/about_service">このサイトについて</nuxt-link></li>
+            <li><nuxt-link to="/news">お知らせ</nuxt-link></li>
+            <li><nuxt-link to="/terms_of_service">利用規約</nuxt-link></li>
+            <li><nuxt-link to="/privacy_policy">プライバシーポリシー</nuxt-link></li>
           </ul>
         </v-col>
 
@@ -26,10 +26,10 @@
           <h4>FOLLOW</h4>
           <ul>
             <li>
-              <a v-bind:href="youtubeLink"><v-icon>mdi-youtube</v-icon></a>
+              <a v-bind:href="youtubeLink" target="_blank" rel="noopener noreferrer"><v-icon>mdi-youtube</v-icon></a>
             </li>
             <li>
-              <a v-bind:href="twitterLink"><v-icon>mdi-twitter</v-icon></a>
+              <a v-bind:href="twitterLink" target="_blank" rel="noopener noreferrer"><v-icon>mdi-twitter</v-icon></a>
             </li>
           </ul>
         </v-col>
@@ -37,8 +37,8 @@
         <v-col cols="auto">
           <h4>ABOUT US</h4>
           <ul>
-            <li>開発・運営者について</li>
-            <li>お問い合わせ</li>
+            <li><a href="https://twitter.com/sorarideblog" target="_blank" rel="noopener noreferrer">開発・運営者について</a></li>
+            <li>><nuxt-link to="/contact_form">お問い合わせ</nuxt-link></li>
           </ul>
         </v-col>
       </v-row>
