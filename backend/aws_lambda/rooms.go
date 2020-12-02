e9bbf30fdce5ffa3dfc1f89c39a3a37ac060dc3e
Sorarideblog <54093651+sorarideblog@users.noreply.github.com>
2020-12-02 23:02:02 +0900

    update [api] comment

--- backend/aws_lambda/rooms.go
+++ backend/aws_lambda/rooms.go
@@ -6,19 +6,17 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-
 type RoomsResponseStruct struct {
 	Result  string       `json:"result"`
 	Message string       `json:"message"`
 	Rooms   []RoomStruct `json:"rooms"`
 }
 
-
 func Rooms(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	ctx, client := InitializeHttpFunc()
 	defer client.Close()
 
-	UpdateDatabase(client, ctx)
+	UpdateDatabase(client, ctx) // todo 頻繁にアクセスされるこの関数で毎回するには適さない。読み込みが増えすぎる。
 
 	var apiResp RoomsResponseStruct
 
