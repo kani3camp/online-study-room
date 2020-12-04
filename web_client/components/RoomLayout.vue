76b21d699886cadd7cf401b8bdca73c1e0db1643
Sorarideblog <54093651+sorarideblog@users.noreply.github.com>
2020-12-05 03:23:20 +0900

    temp

--- web_client/components/RoomLayout.vue
+++ web_client/components/RoomLayout.vue
@@ -13,18 +13,26 @@ export default {
     maxDisplayNameLength: 6,
   }),
   async created() {
-    const url = new URL('https://io551valj4.execute-api.ap-northeast-1.amazonaws.com/room_layout')
-    url.search = new URLSearchParams({
-      room_id: 'english-room',
-    }).toString()
-    const response = await fetch(url.toString(), { method: 'GET' })
-    const res_html = await response.text()
+    // const url = new URL('https://io551valj4.execute-api.ap-northeast-1.amazonaws.com/room_layout')
+    // url.search = new URLSearchParams({
+    //   room_id: 'english-room',
+    // }).toString()
+    // const response = await fetch(url.toString(), { method: 'GET' })
+    // const res_html = await response.body.room_layout_data
+    // console.log('body: ', response.body)
+    const res = await common.httpGet(
+      'https://io551valj4.execute-api.ap-northeast-1.amazonaws.com/room_layout',
+      { room_id: 'english-room' }
+    )
+    const res_html = res.room_layout_data
+    console.log(res_html)
     const dom_parser = new DOMParser()
     try {
-      const layout_doc = dom_parser.parseFromString(this.escape_html(res_html), 'text/html')
-      document.getElementById('room-layout').appendChild(layout_doc.body)
+      const layout_doc = dom_parser.parseFromString(res_html, 'text/html')
+      const roomLayoutDiv = document.getElementById('room-layout')
+      await roomLayoutDiv.appendChild(layout_doc.documentElement)
       console.log('どう？')
-      console.log(layout_doc)
+      console.log(layout_doc.getElementsByClassName('group').length)
     } catch (e) {
       console.error(e)
     }
@@ -63,4 +71,9 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+#room-layout {
+  height: content-box;
+  width: content-box;
+}
+</style>
--- web_client/pages/index.vue
+++ web_client/pages/index.vue
@@ -4,9 +4,6 @@
 
     <ToolBar />
 
-    <!--    todo-->
-    <!--    <RoomLayout />-->
-
     <v-main>
       <RoomLayout />
 
