1659e9e85494add8f85fcedd1331917c94216bc9
Sorarideblog <54093651+sorarideblog@users.noreply.github.com>
2020-12-05 01:54:46 +0900

    temp

--- web_client/components/RoomLayout.vue
+++ web_client/components/RoomLayout.vue
@@ -1,9 +1,9 @@
 <template>
-  <!--  todo このdivは消す-->
-  <iframe src="../assets/english-room.html" />
+  <div id="room-layout" />
 </template>
 
 <script>
+import common from '@/plugins/common'
 export default {
   name: 'RoomLayout',
   props: [],
@@ -12,8 +12,34 @@ export default {
     emptySeatColor: '#ddcec3',
     maxDisplayNameLength: 6,
   }),
-  mounted() {},
+  async created() {
+    const url = new URL('https://io551valj4.execute-api.ap-northeast-1.amazonaws.com/room_layout')
+    url.search = new URLSearchParams({
+      room_id: 'english-room',
+    }).toString()
+    const response = await fetch(url.toString(), { method: 'GET' })
+    const res_html = await response.text()
+    const dom_parser = new DOMParser()
+    const layout_doc = dom_parser.parseFromString(this.escape_html(res_html), 'text/html')
+    document.getElementById('room-layout').appendChild(layout_doc.body)
+    console.log('どう？')
+  },
   methods: {
+    escape_html(string) {
+      if (typeof string !== 'string') {
+        return string
+      }
+      return string.replace(/[&'`"<>]/g, function (match) {
+        return {
+          '&': '&amp;',
+          "'": '&#x27;',
+          '`': '&#x60;',
+          '"': '&quot;',
+          '<': '&lt;',
+          '>': '&gt;',
+        }[match]
+      })
+    },
     window: (onload = function () {
       const numSeats = 23
 
