bbcb6de67b3fe7950d86a4509ca06aeaf5f1eeed
Sorarideblog <54093651+sorarideblog@users.noreply.github.com>
2020-12-05 12:42:20 +0900

    temp [web] #61

--- web_client/components/RoomLayout.vue
+++ web_client/components/RoomLayout.vue
@@ -1,57 +1,118 @@
 <template>
-  <div id="room-layout" />
+  <div id="room-layout">
+    <!--    <Seat-->
+    <!--      v-for="seat in layout.seats"-->
+    <!--      :key="seat.id"-->
+    <!--      :x="seat.x"-->
+    <!--      :y="seat.y"-->
+    <!--    />-->
+    <div
+      v-for="(seat, index) in layout.seats"
+      :key="seat.id"
+      class="seat"
+      :style="{ backgroundColor: emptySeatColor, top: seatPositions[index].x+'%', left: seatPositions[index].y+'%', width: seatShape.width+'%' }"
+    >
+      {{ seat.id }}
+    </div>
+  </div>
 </template>
 
 <script>
 import common from '@/plugins/common'
+import Seat from '@/components/Seat'
+
 export default {
   name: 'RoomLayout',
+  comments: {
+    Seat,
+  },
   props: [],
   data: () => ({
-    layoutFile: '@/assets/english-room.html',
-    emptySeatColor: '#ddcec3',
-    maxDisplayNameLength: 6,
+    emptySeatColor: '#c36b2b',
+    layout: {
+      room_id: 'mathematics-room',
+      'layout-version': 1,
+      'num-seats': 3,
+      'room-shape': {
+        width: 1000,
+        height: 800,
+      },
+      'seat-shape': {
+        width: 120,
+        height: 100,
+      },
+      seats: [
+        {
+          id: 1,
+          x: 0,
+          y: 100,
+        },
+        {
+          id: 2,
+          x: 0,
+          y: 200,
+        },
+        {
+          id: 3,
+          x: 500,
+          y: 0,
+        },
+      ],
+      partitions: [
+        {
+          id: 1,
+          x: 120,
+          y: 80,
+        },
+        {
+          id: 2,
+          x: 0,
+          y: 200,
+        },
+        {
+          id: 3,
+          x: 120,
+          y: 280,
+        },
+        {
+          id: 4,
+          x: 0,
+          y: 400,
+        },
+        {
+          id: 5,
+          x: 120,
+          y: 480,
+        },
+        {
+          id: 6,
+          x: 0,
+          y: 600,
+        },
+      ],
+    },
   }),
-  async created() {
-    // const url = new URL('https://io551valj4.execute-api.ap-northeast-1.amazonaws.com/room_layout')
-    // url.search = new URLSearchParams({
-    //   room_id: 'english-room',
-    // }).toString()
-    // const response = await fetch(url.toString(), { method: 'GET' })
-    // const res_html = await response.body.room_layout_data
-    // console.log('body: ', response.body)
-    const res = await common.httpGet(
-      'https://io551valj4.execute-api.ap-northeast-1.amazonaws.com/room_layout',
-      { room_id: 'english-room' }
-    )
-    const res_html = res.room_layout_data
-    console.log(res_html)
-    const dom_parser = new DOMParser()
-    try {
-      const layout_doc = dom_parser.parseFromString(res_html, 'text/html')
-      const roomLayoutDiv = document.getElementById('room-layout')
-      await roomLayoutDiv.appendChild(layout_doc.documentElement)
-      console.log('どう？')
-      console.log(layout_doc.getElementsByClassName('group').length)
-    } catch (e) {
-      console.error(e)
-    }
-  },
-  methods: {
-    escape_html(string) {
-      if (typeof string !== 'string') {
-        return string
+  computed: {
+    seatShape: function () {
+      return {
+        width: (100 * this.layout['seat-shape'].width) / this.layout['room-shape'].width,
+        height: (100 * this.layout['seat-shape'].height) / this.layout['room-shape'].height,
       }
-      return string.replace(/[&'`"<>]/g, function (match) {
-        return {
-          '&': '&amp;',
-          "'": '&#x27;',
-          '`': '&#x60;',
-          '"': '&quot;',
-          '<': '&lt;',
-          '>': '&gt;',
-        }[match]
+    },
+    seatPositions: function () {
+      const vm = this
+      return this.layout.seats.map(function (seat) {
+        return vm.seatPosition(seat.x, seat.y)
       })
+    },
+  },
+  async created() {},
+  methods: {
+    seatPosition(x, y) {
+      return {
+        x: (100 * x) / this.layout['room-shape'].width,
+        y: (100 * y) / this.layout['room-shape'].height,
+      }
     },
     window: (onload = function () {
       const numSeats = 23
@@ -73,7 +134,16 @@ export default {
 
 <style scoped>
 #room-layout {
-  height: content-box;
-  width: content-box;
+  /*todo*/
+  width: 1000px;
+  height: 800px;
+  background-color: azure;
+  margin: auto;
+}
+
+.seat {
+  width: 4rem;
+  height: 3rem;
+  position: relative;
 }
 </style>
---
+++ web_client/components/Seat.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    :id="seatId"
+    class="seat"
+    :style="{ backgroundColor: emptySeatColor }"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Seat',
+  props: {
+    x: {
+      type: Number,
+      default: 0,
+    },
+    y: {
+      type: Number,
+      default: 0,
+    },
+    id: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data: () => ({
+    emptySeatColor: '#c36b2b',
+    maxDisplayNameLength: 6,
+    seatId: 'seat-' + this.id.toString(),
+  }),
+}
+</script>
+
+<style scoped>
+.seat {
+  /*todo*/
+  width: 3rem;
+  height: 2rem;
+  position: relative;
+  top: 0;
+  left: 0;
+}
+</style>
