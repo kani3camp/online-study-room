60da39ad7ee8ff9b579b0affef93482e0d662ab1
kani3camp <sorarideblog@gmail.com>
2024-10-20 18:34:48 +0900

    とりあえず動作する状態にした

--- web_client/plugins/common.js
+++ web_client/plugins/common.js
@@ -6,21 +6,12 @@ const productionApiLink = {
   online_users: '',
   user_status: 'https://io551valj4.execute-api.ap-northeast-1.amazonaws.com/user_status',
   change_user_info: 'https://io551valj4.execute-api.ap-northeast-1.amazonaws.com/change_user_info',
-  send_contact_form: 'https://io551valj4.execute-api.ap-northeast-1.amazonaws.com/send_contact_form',
+  send_contact_form:
+    'https://io551valj4.execute-api.ap-northeast-1.amazonaws.com/send_contact_form',
   create_new_room: '',
   news: 'https://io551valj4.execute-api.ap-northeast-1.amazonaws.com/news',
   room_layout: '',
   websocket: 'wss://0ieer51ju9.execute-api.ap-northeast-1.amazonaws.com/release',
-}
-const productionFirebaseConfig = {
-  apiKey: 'AIzaSyCIY7RCArApaRpg4nkVUJCze88OmF6-wGM',
-  authDomain: 'online-study-space.firebaseapp.com',
-  databaseURL: 'https://online-study-space.firebaseio.com',
-  projectId: 'online-study-space',
-  storageBucket: 'online-study-space.appspot.com',
-  messagingSenderId: '486366182751',
-  appId: '1:486366182751:web:dc94794117a6268a050aea',
-  measurementId: 'G-KH6EFLWF7T',
 }
 
 const testApiLink = {
@@ -28,21 +19,14 @@ const testApiLink = {
   room_status: 'https://fquvf774q5.execute-api.ap-northeast-1.amazonaws.com/test_room_status',
   online_users: '',
   user_status: 'https://fquvf774q5.execute-api.ap-northeast-1.amazonaws.com/test_user_status',
-  change_user_info: 'https://fquvf774q5.execute-api.ap-northeast-1.amazonaws.com/test_change_user_info',
-  send_contact_form: 'https://fquvf774q5.execute-api.ap-northeast-1.amazonaws.com/test_send_contact_form',
+  change_user_info:
+    'https://fquvf774q5.execute-api.ap-northeast-1.amazonaws.com/test_change_user_info',
+  send_contact_form:
+    'https://fquvf774q5.execute-api.ap-northeast-1.amazonaws.com/test_send_contact_form',
   create_new_room: '',
   news: 'https://fquvf774q5.execute-api.ap-northeast-1.amazonaws.com/test_news',
   room_layout: '',
   websocket: 'wss://jukb7tixp7.execute-api.ap-northeast-1.amazonaws.com/test',
-}
-const testFirebaseConfig = {
-  apiKey: 'AIzaSyAE5Hm9U0pQDxXAPNkmthO_z--CFDiSn6w',
-  authDomain: 'test-online-study-space.firebaseapp.com',
-  projectId: 'test-online-study-space',
-  storageBucket: 'test-online-study-space.appspot.com',
-  messagingSenderId: '788678710894',
-  appId: '1:788678710894:web:2cb434fbd57cdc9bfd787f',
-  measurementId: 'G-450CVZ6FWP',
 }
 
 const common = {
@@ -50,12 +34,10 @@ const common = {
     youtubeLink: 'https://www.youtube.com/channel/UCXuD2XmPTdpVy7zmwbFVZWg',
     twitterLink: 'https://twitter.com/osr_soraride',
   },
-  // todo デプロイ時に変更
+  // TODO: デプロイ時に変更
   // apiLink: productionApiLink,
-  // firebaseConfig: productionFirebaseConfig,
 
   apiLink: testApiLink,
-  firebaseConfig: testFirebaseConfig,
 }
 
 common.c = (m) => {
